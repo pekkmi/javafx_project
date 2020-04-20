98daa7018c0f0ddaa481964848cc2d3454b77154
pekkmi <pekkmi@gmail.com>
2020-04-20 11:36:17 +0300

    made changes to ui style

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -1,15 +1,25 @@
 package com.company;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) {
         stage.setTitle("JavaFX code editor by:Mika Pekkala ");
+        stage.initStyle(StageStyle.DECORATED);
+        stage.setResizable(true);
         stage.show();
+        Scene clear = new Scene(new Button("Click!"));
+        stage.setScene(clear);
+
+
     }
     public static void main(String args[]) {
         launch(args);
     }
+
 }
