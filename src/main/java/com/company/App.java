d7df9ec3b6c2c85aac56ed3bb1b1c11ab819ec19
pekkmi <pekkmi@gmail.com>
2020-04-20 10:53:47 +0300

    added space after title

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -6,7 +6,7 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage stage) {
-        stage.setTitle("JavaFX code editor by:Mika Pekkala");
+        stage.setTitle("JavaFX code editor by:Mika Pekkala ");
         stage.show();
     }
     public static void main(String args[]) {
