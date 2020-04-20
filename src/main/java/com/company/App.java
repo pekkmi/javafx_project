bbaf3cad7930a85ca38992b0f4ecb5910e5464f1
pekkmi <pekkmi@gmail.com>
2020-04-20 15:35:30 +0300

    continuing...

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -3,9 +3,9 @@ package com.company;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -17,18 +17,35 @@ public class App extends Application {
     public void start(Stage stage) {
         stage.setTitle("JavaFX code editor by:Mika Pekkala ");
         stage.initStyle(StageStyle.DECORATED);
-        TextArea tekstikentta = new TextArea();
         BorderPane layout = new BorderPane();
-        layout.setCenter(tekstikentta);
+        TextArea tekstikentta = new TextArea();
+        MenuBar valikko = new MenuBar();
+
         Button clearText = new Button("Clear");
         Button click2 = new Button("Click2!");
         HBox ylapalkki = new HBox(clearText, click2);
         layout.setTop(ylapalkki);
-        clearText.setOnAction(new EventHandler<ActionEvent>() {
-            @Override public void handle(ActionEvent e) {
-                tekstikentta.clear();
-            }
-        });
+        clearText.setOnAction(e -> tekstikentta.clear());
+
+        VBox menus = new VBox(valikko, ylapalkki);
+
+        layout.setCenter(tekstikentta);
+
+        Menu menu1 = new Menu("File");
+        valikko.getMenus().add(menu1);
+        layout.setTop(menus);
+        MenuItem menuItem1 = new MenuItem("Item 1");
+        MenuItem menuItem2 = new MenuItem("Item 2");
+        MenuItem separator = new MenuItem("------");
+        MenuItem exit = new MenuItem("Exit");
+
+        menu1.getItems().add(menuItem1);
+        menu1.getItems().add(menuItem2);
+        menu1.getItems().add(separator);
+        menu1.getItems().add(exit);
+
+        exit.setOnAction(e -> System.exit(0));
+
         Scene content = new Scene(layout,680, 460);
         stage.setScene(content);
         stage.show();
