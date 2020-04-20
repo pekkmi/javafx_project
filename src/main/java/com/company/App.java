212818b5dd5df86abf72765d7758955006c90fc0
pekkmi <pekkmi@gmail.com>
2020-04-20 15:51:30 +0300

    continuing...

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -1,9 +1,6 @@
 package com.company;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
