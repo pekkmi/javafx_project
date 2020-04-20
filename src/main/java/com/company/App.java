bb118fb5f26b2b0a24d70922998ced47c2859605
pekkmi <pekkmi@gmail.com>
2020-04-20 14:42:28 +0300

    made changes to ui style

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -1,8 +1,14 @@
 package com.company;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -11,10 +17,21 @@ public class App extends Application {
     public void start(Stage stage) {
         stage.setTitle("JavaFX code editor by:Mika Pekkala ");
         stage.initStyle(StageStyle.DECORATED);
-        stage.setResizable(true);
+        TextArea tekstikentta = new TextArea();
+        BorderPane layout = new BorderPane();
+        layout.setCenter(tekstikentta);
+        Button clearText = new Button("Clear");
+        Button click2 = new Button("Click2!");
+        HBox ylapalkki = new HBox(clearText, click2);
+        layout.setTop(ylapalkki);
+        clearText.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                tekstikentta.clear();
+            }
+        });
+        Scene content = new Scene(layout,680, 460);
+        stage.setScene(content);
         stage.show();
-        Scene clear = new Scene(new Button("Click!"));
-        stage.setScene(clear);
 
 
     }
